b5e095193cd52b9b0b27eee4d5caaab7f984fe3a
ELDOUQE ABDERRAZAK <a.eldouque@fc.ueuromed.org>
2022-12-04 17:44:39 +0100

    Change name shared  to _shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,10 @@ import { ConatinerStockTrackerComponent } from './components/conatiner-stock-tra
 import { FormStockComponent } from './components/conatiner-stock-tracker/form-stock/form-stock.component';
 import { DashboardComponent } from './components/conatiner-stock-tracker/dashboard/dashboard.component';
 import { CardStockStatsComponent } from './components/conatiner-stock-tracker/dashboard/card-stock-stats/card-stock-stats.component';
-import { CandleComponent } from './shared/components/icons/candle/candle.component';
+import { CandleComponent } from './_shared/components/icons/candle/candle.component';
 import { FinnhubInterceptor } from './_core/interceptors/finnhub.interceptor';
-import { CloseComponent } from './shared/components/icons/close/close.component';
-import { ArrowStatsComponent } from './shared/components/icons/arrow-stats/arrow-stats.component';
+import { CloseComponent } from './_shared/components/icons/close/close.component';
+import { ArrowStatsComponent } from './_shared/components/icons/arrow-stats/arrow-stats.component';
 
 @NgModule({
   declarations: [
